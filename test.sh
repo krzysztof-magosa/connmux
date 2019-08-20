9e7dfc0f53a7f7c4288f38269ba68a8cd92dffe6
Krzysztof Magosa <krzysztof@magosa.pl>
2019-08-20 18:13:51 +0200

    fix script name in test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # coproc requires bash 4.x
-coproc python main.py noc.pirxnet.pl 80 client
-python main.py 0.0.0.0 9999 server <&${COPROC[0]} >&${COPROC[1]}
+coproc python connmux.py noc.pirxnet.pl 80 client
+python connmux.py 0.0.0.0 9999 server <&${COPROC[0]} >&${COPROC[1]}
