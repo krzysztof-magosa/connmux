12dbc6609041627bca0632c3603c905f280933b0
Krzysztof Magosa <krzysztof@magosa.pl>
2019-08-20 18:45:02 +0200

    log during tests

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # coproc requires bash 4.x
-coproc python connmux.py noc.pirxnet.pl 80 client
-python connmux.py 0.0.0.0 9999 server <&${COPROC[0]} >&${COPROC[1]}
+coproc python connmux.py noc.pirxnet.pl 80 client 2>./client.log
+python connmux.py 0.0.0.0 9999 server <&${COPROC[0]} >&${COPROC[1]} 2>./server.log
